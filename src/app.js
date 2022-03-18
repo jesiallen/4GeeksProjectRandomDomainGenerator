899fc06f6cb762fcc3020a603ea75ee8e0c44fb8
jesiallen <justjesi2010@gmail.com>
2022-03-18 23:40:54 +0000

    Simple JavaScript randomizer.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,44 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = [
+    "who",
+    "whom",
+    "whose",
+    "which",
+    "that",
+    "what",
+    "whatever",
+    "whoever",
+    "whomever",
+    "whichever"
+  ];
+  let adj = [
+    "absurd",
+    "bewildered",
+    "cloistered",
+    "chivalrous",
+    "debonair",
+    "enchanting",
+    "floppy",
+    "grandiose"
+  ];
+  let noun = [
+    "afternoon",
+    "rainbow",
+    "parrot",
+    "kangaroo",
+    "fox",
+    "vegetable",
+    "notebook",
+    "tree"
+  ];
+
+  let newPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let newAdj = adj[Math.floor(Math.random() * adj.length)];
+  let newNoun = noun[Math.floor(Math.random() * noun.length)];
+
+  document.querySelector(
+    ".alert"
+  ).innerHTML = `${newPronoun}${newAdj}${newNoun}.com`;
 };
